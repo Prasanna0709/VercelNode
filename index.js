89fcdc93d7ca70518cd54f8e0b9ec6bc7d0437bc
prasanna0218 <prasannampmrasanna@gmail.com>
2025-04-03 22:39:07 +0530

    som

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/fget",(req,res)=>{
     res.send("First get!");
 });
 
+app.post("/mypost",()=>{
+    console.log("my app");
+})
+
 app.listen(3000,()=>{
     console.log("App is running on Port 3000");
 });
